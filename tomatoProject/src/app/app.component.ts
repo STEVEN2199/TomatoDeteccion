56722368d103bb67f791147c6b8fd5b836f8b170
STEVEN2199 <stevenguillermo_encalada@hotmail.com>
2024-01-06 13:44:17 -0500

    acople con modelo
    acople con modelo

--- tomatoProject/src/app/app.component.ts
+++ tomatoProject/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import * as tf from '@tensorflow/tfjs';
 
 @Component({
@@ -8,43 +8,71 @@ import * as tf from '@tensorflow/tfjs';
 })
 export class AppComponent implements OnInit {
   model!: tf.LayersModel;
-  /*imagenUrl: string | ArrayBuffer;
-  defectosDetectados: string;
-  porcentajeDefecto: number;*/
+  imagenUrl: string | ArrayBuffer;
+  resultado: string;
+  porcentaje: number;
+  cargando: boolean = false;
+
+  constructor(private cd: ChangeDetectorRef) {} // Inyecta ChangeDetectorRef
 
   ngOnInit() {
     this.loadModel();
   }
 
   async loadModel() {
-    this.model = await tf.loadLayersModel('ruta/a/tu/modelo.json');
+    this.model = await tf.loadLayersModel('../assets/model.json');
   }
 
-  /*predict(imageData) {
-    const imageTensor = tf.browser.fromPixels(imageData).toFloat();
-    const normalized = imageTensor.div(tf.scalar(255.0));
-    const input = normalized.reshape([1, ...normalized.shape]);
-
-
-    const prediction = this.model.predict(input);
-    return prediction.arraySync();
-  }*/
-
-  /*onFileSelected(event) {
-    const file: File = event.target.files[0];
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
 
     if (file) {
+      this.cargando = true; // Comienza la carga
+
       const reader = new FileReader();
-      //reader.onload = e => this.imagenUrl = reader.result;
+      reader.onload = async e => {
+        this.imagenUrl = reader.result as string;
+        this.cd.detectChanges(); // Detecta los cambios
+
+        this.cargando = false; // Termina la carga
+      }
       reader.readAsDataURL(file);
     }
-  }*/
+  }
 
-  onUpload() {
-    // Aquí puedes añadir la lógica para enviar la imagen al servidor y obtener los resultados del análisis de la CNN.
-    // Una vez que obtengas los resultados, puedes mostrarlos en la vista.
-    // Actualiza defectosDetectados y porcentajeDefecto según los resultados del análisis.
-    /*this.defectosDetectados = "ManchasColor";
-    this.porcentajeDefecto = 47.86;*/
+  async preprocessImage(imageSrc: string) {
+    const img = new Image();
+    img.src = imageSrc;
+    await img.decode(); // Espera a que se cargue la imagen
+    let tensor = tf.browser.fromPixels(img)
+      .resizeNearestNeighbor([224, 224]) // Redimensiona la imagen
+      .toFloat()
+      .expandDims(0); // Añade una dimensión de lote al principio
+
+    return tensor;
+  }
+
+  async detectDefects() {
+    // Preprocesa la imagen y conviértela en un tensor
+    const tensor = await this.preprocessImage(this.imagenUrl as string);
+
+    // Haz la predicción
+    const prediction = this.model.predict(tensor) as tf.Tensor;
+
+    // Aquí puedes procesar la predicción como necesites
+    // Por ejemplo, puedes encontrar el índice de la clase con la mayor probabilidad
+    const result = prediction.argMax(1).dataSync()[0];
+
+    // Y luego asignar el resultado y el porcentaje a tus propiedades
+    this.resultado = `Clase ${result}`;
+    this.porcentaje = prediction.max().dataSync()[0] * 100;
+
+    this.cd.detectChanges(); // Detecta los cambios
+  }
+
+  limpiar() {
+    this.imagenUrl = '';
+    this.resultado = '';
+    this.porcentaje = 0;
   }
 }
